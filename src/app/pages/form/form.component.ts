343bffafb4c21f0426b596cc5b3f478aa7fe5ddc
asam3 <asam3000m@gmail.com>
2023-02-23 10:26:05 +0330

    remove unused code

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Observable } from "rxjs";
-import { FieldModel } from "../../fields-model/field-model";
-import { FieldService } from "../../services/field.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FieldModel } from '../../fields-model/field-model';
+import { FieldService } from '../../services/field.service';
 
 @Component({
-  selector: "app-form",
-  templateUrl: "./form.component.html",
-  styleUrls: ["./form.component.css"],
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-  questions$: Observable<FieldModel<any>[]>;
   fields: FieldModel<any>[] = [];
   formId;
   error = false;
@@ -18,9 +17,8 @@ export class FormComponent implements OnInit {
     private fieldService: FieldService,
     private route: ActivatedRoute
   ) {
-    this.questions$ = fieldService.getQuestions();
     this.route.params.subscribe((param) => {
-      this.formId = param["formId"];
+      this.formId = param['formId'];
     });
   }
 
@@ -29,7 +27,7 @@ export class FormComponent implements OnInit {
 
     const form = this.fieldService.getForm(this.formId);
     form.then((data) => {
-      this.fields = data["fields"];
+      this.fields = data['fields'];
     });
 
     form.catch((error) => {
--- src/app/services/field.service.ts
+++ src/app/services/field.service.ts
@@ -1,88 +1,20 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
-import { DropDown } from "../fields-model/drop-down";
-import { FieldModel } from "../fields-model/field-model";
-import { TextBox } from "../fields-model/text-box";
-import { TextArea } from "../fields-model/text-area";
-import { of } from "rxjs";
-import { NumberBox } from "../fields-model/number-box";
-import { RadioButton } from "../fields-model/radio-button";
-import { FormModel } from "../fields-model/form-model";
+import { DropDown } from '../fields-model/drop-down';
+import { FieldModel } from '../fields-model/field-model';
+import { TextBox } from '../fields-model/text-box';
+import { TextArea } from '../fields-model/text-area';
+import { of } from 'rxjs';
+import { NumberBox } from '../fields-model/number-box';
+import { RadioButton } from '../fields-model/radio-button';
+import { FormModel } from '../fields-model/form-model';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class FieldService {
   fields: FieldModel<string>[] = [];
   forms: FormModel<any>[] = [];
-  getQuestions() {
-    const fields: FieldModel<string>[] = [
-      // new DropDown({
-      //   key: 'brave',
-      //   label: 'Bravery Rating',
-      //   options: [
-      //     { key: 'solid', value: 'Solid' },
-      //     { key: 'great', value: 'Great' },
-      //     { key: 'good', value: 'Good' },
-      //     { key: 'unproven', value: 'Unproven' },
-      //   ],
-      //   order: 3,
-      // }),
-      new RadioButton({
-        key: "sex",
-        label: "Sex",
-        type: "radio",
-        options: [
-          { key: "Male", value: "m" },
-          { key: "Female", value: "f" },
-        ],
-        required: true,
-        order: 4,
-      }),
-      new TextBox({
-        key: "firstName",
-        label: "First name",
-        value: "Bombasto",
-        required: true,
-        order: 1,
-      }),
-      // new TextArea({
-      //   key: 'area',
-      //   label: '',
-      //   value: 'I am text area',
-      //   required: true,
-      //   order: 10,
-      // }),
-      // new TextBox({
-      //   key: 'number',
-      //   label: 'num',
-      //   value: 'I am text area',
-      //   type: 'number',
-      //   required: true,
-      //   order: 10,
-      // }),
-      // new TextBox({
-      //   key: 'emailAddress',
-      //   label: 'Email',
-      //   type: 'email',
-      //   order: 1,
-      // }),
-      // new TextBox({
-      //   key: 'date',
-      //   label: 'date',
-      //   type: 'date',
-      //   order: 1,
-      // }),
-      // new TextBox({
-      //   key: 'check',
-      //   label: 'checkbox',
-      //   type: 'checkbox',
-      //   order: 4,
-      // }),
-    ];
-
-    return of(fields.sort((a, b) => a.order - b.order));
-  }
 
   /**
    * return form array. but in real condition it should come from server
@@ -104,7 +36,7 @@ export class FieldService {
       if (form) {
         resolve(form);
       } else {
-        reject("No form found");
+        reject('No form found');
       }
     });
   }
